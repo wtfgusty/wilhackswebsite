0efbf7c214f0f3fc247520af0286c8af9255ee21
gusty <74928415+wtfgusty@users.noreply.github.com>
2023-03-31 22:16:31 -0700

    Delete wilhacks project directory

--- wilhacks project/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-function aboutPage() {
-    location.href = "./aboutUs.html"
-};
-function omPage() {
-    location.href = "./ourMission.html"
-}
-function donatePage() {
-    location.href = "./donate.html"
-}
